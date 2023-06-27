c3f71ec847db4d9ffd624a54251bed6dba201bbb
Javlonbek Abdurasulov <javlonbeabr41@gmail.com>
2023-06-27 13:53:38 +0500

    fix

--- movies.js
+++ movies.js
@@ -1,9 +1,5 @@
 let allVideos = [
     {
-        name: 'Нашид - Nasheed Axiy Anta Xurrun Очень красивый нашиды 🥀🎧',
-        url:'https://www.youtube.com/embed/KDJXGcGNZq4'
-    },
-    {
         name: 'Molu davlatni topgaysan ota onang topilmaydi',
         url: 'https://www.youtube.com/embed/Lg6VyFs9hIU'
     }
